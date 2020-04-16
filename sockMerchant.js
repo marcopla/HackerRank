4294e98b006b712f7097781a8cec666296dd31ff
Marco <marco.pla@gmail.com>
2020-04-15 21:16:02 -0300

    improved

--- sockMerchant.js
+++ sockMerchant.js
@@ -1,18 +1,19 @@
 
 //const n = 9;
 //let array = [10, 20, 20, 10, 10, 30, 50, 10, 20];
-const n = 15;
-let array = [6, 5, 2, 3, 5, 2, 2, 1, 1, 5, 1, 3, 3, 3, 5];
+//const n = 15;
+//let array = [6, 5, 2, 3, 5, 2, 2, 1, 1, 5, 1, 3, 3, 3, 5];
+const n = 100;
+let string = '44 55 11 15 4 72 26 91 80 14 43 78 70 75 36 83 78 91 17 17 54 65 60 21 58 98 87 45 75 97 81 18 51 43 84 54 66 10 44 45 23 38 22 44 65 9 78 42 100 94 58 5 11 69 26 20 19 64 64 93 60 96 10 10 39 94 15 4 3 10 1 77 48 74 20 12 83 97 5 82 43 15 86 5 35 63 24 53 27 87 45 38 34 7 48 24 100 14 80 54';
+
+let array = string.split(' ');
+console.log(array); 
+console.log(array.length); 
+
+let pairs = sockMerchant(n, array);
+console.log(`Total de pares de meias possíveis: ${pairs}`);
 
 
-    let pairs = sockMerchant(n, array);
-    console.log(`Total de pares de meias possíveis: ${pairs}`);
-
-   //let array2 = 
-   let string = '44 55 11 15 4 72 26 91 80 14 43 78 70 75 36 83 78 91 17 17 54 65 60 21 58 98 87 45 75 97 81 18 51 43 84 54 66 10 44 45 23 38 22 44 65 9 78 42 100 94 58 5 11 69 26 20 19 64 64 93 60 96 10 10 39 94 15 4 3 10 1 77 48 74 20 12 83 97 5 82 43 15 86 5 35 63 24 53 27 87 45 38 34 7 48 24 100 14 80 54';
-    arrString = string.split(' ');
-    console.log(arrString); 
-    console.log(string); 
 
 
 
